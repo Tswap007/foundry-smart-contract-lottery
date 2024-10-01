9c73fdf489041b71de24ec8ca05c1cf47c3c7cdb
tswap <tswap700@gmail.com>
2024-10-01 23:30:04 +0100

    Initializing layout of contract

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,6 +2,31 @@
 
 pragma solidity ^0.8.19;
 
+/**
+ * @title A simple raffle contract 
+ * @author PickleRick 
+ * @notice This contract is a simple raffle contract
+ * @dev Implements Chainlink VRFv2.5
+ */
 contract Raffle {
-    
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable{
+        
+    }
+
+    function pickWinner() public {
+        // Pick the winner
+    }
+
+    /**
+     * Getter functions
+     */
+    function getEntranceFee() external view returns(uint256) {
+        return i_entranceFee;
+    }
 }
